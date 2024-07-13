92bea3f5f6e57b602ebefb1a4eea16fb2ffe2e83
Ignasi Camps <icampsilla@gmail.com>
2024-07-13 21:40:38 +0200

    updated copyright marks

--- finder/test/test_CFormula.cpp
+++ finder/test/test_CFormula.cpp
@@ -1,3 +1,11 @@
+/* =================================================== *
+ * ====         Copyright (c) 2024 icampsi        ==== *
+ * ==== SPDX-License-Identifier: GPL-3.0-or-later ==== *
+ * =================================================== */
+
 #include <catch2/catch_all.hpp>
+
+// BOOKMARK - NEEDS IMPLEMENTATION
+
 TEST_CASE("3") {
 }
--- poppler_interface/test/test_PdfToText.cpp
+++ poppler_interface/test/test_PdfToText.cpp
@@ -1,3 +1,8 @@
+/* =================================================== *
+ * ====        Copyright (c) 2024 icampsi       ==== *
+ * ==== SPDX-License-Identifier: GPL-3.0-or-later ==== *
+ * =================================================== */
+
 #include <catch2/catch_all.hpp>
 
 // #include "../PdfToText.h"
